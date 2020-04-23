6a567188f967db7f8484d124eb846cbfa38bec01
nadav-lavy-exa <nadav@exabeam.com>
2020-04-23 22:53:49 +0300

    fully working webhook that sends to sqs

---
+++ handler.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const {eventsQueue} = process.env;
+const SQS = new AWS.SQS();
+
+module.exports.hello = async event => {
+    try {
+        await SQS.sendMessage({
+            MessageBody: JSON.stringify(event.body),
+            QueueUrl: eventsQueue
+        }).promise();
+
+        // for debug only.
+        return {
+            statusCode: 200,
+            body: JSON.stringify(
+                {
+                    message: `Event sent to queue ${eventsQueue}`,
+                    input: event,
+                },
+                null,
+                2
+            ),
+        };
+        // for debug only.
+
+        // for prod.
+        // return {
+        //     statusCode: 200,
+        //     body: `event sent to queue [${event.body}}]`
+        // };
+        // for prod.
+
+    } catch (e) {
+        console.error(`Failed to send message to queue [${eventsQueue}], message [${JSON.stringify(event)}]`, e);
+    }
+};
